859004f33fc09a73132e53346ba38137dd825ce3
Justin Dennis <jxd8037@g.rit.edu>
2021-06-04 20:17:34 -0400

    Added Entity Stats custom inspector and updated package

---
+++ UnityPackages/Assets/MovementSystem/Editor/MSEntityStatsEditor.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace MovementSystem
+{
+    [CustomEditor(typeof(MSEntityStats))]
+    public class MSEntityStatsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            //Move Settings
+            GUIContent label = new GUIContent();
+            label.text = "Move Speed";
+            label.tooltip = "The speed that the entity moves at by default.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("moveSpeed"), label);
+
+            label.text = "Sprint Speed";
+            label.tooltip = "The speed that the entity moves at while sprinting.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("sprintSpeed"), label);
+
+            label.text = "Acceleration Time";
+            label.tooltip = "The target time for it to take the entity to move at its desired velocity.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("accelerationTime"), label);
+
+            label.text = "Move Force";
+            label.tooltip = "The maximum force to use for this entity's movement";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("moveForce"), label);
+
+            //Variable Move Speed Settings
+            label.text = "Variable Move Speed";
+            label.tooltip = "Whether or not movespeed is affected by the entity's forward direction.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("variableSpeed"), label);
+            if (serializedObject.FindProperty("variableSpeed").boolValue)
+            {
+                label.text = "Strafe Speed Multiplier";
+                label.tooltip = "Sideways movement multiplier.";
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("strafeSpeedMult"), label);
+
+                label.text = "Backward Speed Multiplier";
+                label.tooltip = "Backwards speed multiplier.";
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("backSpeedMult"), label);
+
+                label.text = "Max Forward Angle";
+                label.tooltip = "The maximum angle from the transform's forward to count as forward movement in degrees.";
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("forwardAngle"), label);
+
+                label.text = "Max Backward Angle";
+                label.tooltip = "The maximum angle from the transform's back to count as forward movement in degrees.";
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("backwardsAngle"), label);
+            }
+
+            label.text = "Can Fly";
+            label.tooltip = "Whether or not the entity can fly, if true the entity can apply move forces in the y axis.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("canFly"), label);
+
+            label.text = "Jump Velocity";
+            label.tooltip = "The upwards velocity to apply when jumping.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("jumpVelocity"), label);
+
+            label.text = "Dash Speed";
+            label.tooltip = "The default speed of the dash.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("dashSpeed"), label);
+
+            label.text = "Dash Time";
+            label.tooltip = "The amount of time taken to dash.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("dashTime"), label);
+
+            label.text = "Variable Dash Speed";
+            label.tooltip = "Whether or not the dash speed changes over the course of a dash. Toggle on to enable dash speed curve.";
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("variableDashSpeed"), label);
+
+            if (serializedObject.FindProperty("variableDashSpeed").boolValue)
+            {
+                label.text = "Dash Speed Over Time";
+                label.tooltip = "Curve indicating the speed of the dash over its length. X of 0 is the start of the dash, X of 1 is the end, Y of 1 is dash speed, Y of 0 means there is no movement.";
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("dashSpeedOverTime"), label);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
--- UnityPackages/Assets/MovementSystem/MSEntityStats.cs
+++ UnityPackages/Assets/MovementSystem/MSEntityStats.cs
@@ -7,50 +7,34 @@ namespace MovementSystem
     [CreateAssetMenu(fileName = "NewMovementEntityStats", menuName = "MovementSystem/MSEntityStats")]
     public class MSEntityStats : ScriptableObject
     {
-        [Header("Movement Stats")]
-        [Tooltip("The speed that the entity moves at by default.")]
         [SerializeField]
         private float moveSpeed = 2.5f;
-        [Tooltip("Whether or not to change speed based on the direction of motion.")]
         [SerializeField]
         private bool variableSpeed;
-        [Tooltip("Sideways movement multiplier. Only applicable with variable move speed.")]
         [SerializeField]
         private float strafeSpeedMult = 0.75f;
-        [Tooltip("Backwards speed multiplier. Only applicable with variable move speed.")]
         [SerializeField]
         private float backSpeedMult = 0.25f;
-        [Tooltip("The maximum angle from the transform's forward to count as forward movement in degrees.")]
         [SerializeField]
         private float forwardAngle = 60.0f;
-        [Tooltip("The maximum angle from the transform's back to count as forward movement in degrees.")]
         [SerializeField]
         private float backwardsAngle = 30.0f;
-        [Tooltip("The speed that the entity moves at while sprinting.")]
         [SerializeField]
         private float sprintSpeed = 7.5f;
-        [Tooltip("The target time for it to take the entity to move at its desired velocity.")]
         [SerializeField]
         private float accelerationTime = 0.2f;
-        [Tooltip("The amount of force to apply when moving the entity. Lower values may cause the entity to drift, set to 100 by default.")]
         [SerializeField]
         private float moveForce = 100.0f;
-        [Tooltip("The amount of force to apply when jumping.")]
         [SerializeField]
         private float jumpVelocity = 5.0f;
-        [Tooltip("Whether or not the entity can fly, if true the entity can apply move forces in the y axis.")]
         [SerializeField]
         private bool canFly = false;
-        [Tooltip("The default speed of the dash.")]
         [SerializeField]
         private float dashSpeed = 20.0f;
-        [Tooltip("The amount of time taken to dash.")]
         [SerializeField]
         private float dashTime = 0.2f;
-        [Tooltip("Whether or not the dash speed changes over the course of a dash. Toggle on to enable dash speed curve.")]
         [SerializeField]
         private bool variableDashSpeed = true;
-        [Tooltip("Curve indicating the speed of the dash over its length. X of 0 is the start of the dash, X of 1 is the end, Y of 1 is dash speed, Y of 0 means there is no movement.")]
         [SerializeField]
         private AnimationCurve dashSpeedOverTime;
 
